3848f7cb902cc6735e4500184bd5dc27c9eb15b9
Cody Burns <cody.w.burns@gmail.com>
2017-09-01 08:23:44 -0500

    update datefunction
    add human formatted date

--- js/1017.js
+++ js/1017.js
@@ -4,11 +4,14 @@ var uri = 'https://mewapi.epool.io';
 var web3 = new Web3(new Web3.providers.HttpProvider(uri));
 var  currentblk = web3.eth.blockNumber;
 var timeRemains = (5000000 - currentblk) * 14.5;
-var countDownDate = (timeRemains*1000 )  + 604800000 + new Date().getTime();
+var countDownDate = (timeRemains*1000 )  + 604800 + new Date().getTime();
 var progress = (100-((5000000 - currentblk) / 5000000) * 100);
+var real = new Date(countDownDate);
+
+
+document.getElementById("realDate").innerHTML =  formatDate(real);
 
 if(currentblk < 5000000){var currentEra = 5000000}
-
 
 function doGetBlk() {
  currentblk = web3.eth.blockNumber;
@@ -16,7 +19,7 @@ function doGetBlk() {
  progress = (100-((5000000 - currentblk) / 5000000) * 100);
  $('#currentBlock').html(currentblk+"/"+currentEra);
  document.getElementById('era-1').style.width = progress+"%";
- console.log(currentblk);
+
 }
 
 window.onload = function() {
@@ -52,3 +55,28 @@ window.onload = function() {
 	}, 1000);
 
 };
+
+function formatDate(date) {
+  var monthNames = [
+    "January [一月]", "February [二月]", "March [三月]",
+    "April [四月]", "May [五月]", "June [六月]", "July [七月]",
+    "August [八月]", "September [九月]", "October [十月]",
+    "November [十一月]", "December [十二月]"
+  ];
+  var dayNames = [
+    "Sunday [星期天]",
+    "Monday [星期]",
+    "Tuesday [星期二]",
+    "Wednesday [星期三]",
+    "Thursday [星期四]",
+    "Friday [星期五]",
+    "Saturday [星期六]"
+  ]
+
+  var weekday = date.getDay();
+  var day = date.getDate();
+  var monthIndex = date.getMonth();
+  var year = date.getFullYear();
+
+  return dayNames[weekday] +"  - " + day + ' ' + monthNames[monthIndex] + ' ' + year;
+}
